ba6c12b03f60480721b29ee7b056cbf0390a2188
t7abhay <abhaytaras18@gmail.com>
2025-05-24 16:00:09 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import morgan from "morgan";
+
 import cookieParser from "cookie-parser";
 import { rateLimit } from "express-rate-limit";
 import session from "express-session";
@@ -22,7 +22,6 @@ app.use(cors(corsConfig));
 app.use(cookieParser());
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
-app.use(morgan("dev"));
 
 app.set("trust proxy", 1);
 
