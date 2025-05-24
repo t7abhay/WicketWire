1bd7d2c121b4d96b700cc312b3f781da12f4ad47
t7abhay <abhaytaras18@gmail.com>
2025-05-24 15:58:37 +0530

    fix morgan dependency

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.set("trust proxy", 1);
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 100,
+  limit: 150,
   standardHeaders: "draft-8",
   legacyHeaders: false,
   message: "Rate limited ⏱",
